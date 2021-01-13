23749b4e31849737b82ac3624ad3604b19f90c78
josh-thinkpad <josh.owen121@gmail.com>
2021-01-13 20:58:02 +0000

    Add value_price_trend

--- stock_bot.py
+++ stock_bot.py
@@ -50,11 +50,12 @@ class stock_obj():
     """
     Data class for stock information returned by IEX finance API
     """
-    def __init__(self, ticker, sandbox_mode=False):
+    def __init__(self, ticker, sandbox_mode=False, price_key="close"):
         """
         :param str ticker: Stock ticker name (4 letter code)
         :param bool sandbox_mode: Sets API to call from sandbox to preserve message request quota
         """
+        self.price_key = price_key
         self.cloud_box = "sandbox" if sandbox_mode else "cloud"
         self.url_base = f"https://{self.cloud_box}.iexapis.com/stable"
         self.ticker = ticker
@@ -79,6 +80,7 @@ class stock_obj():
         today = today.strftime("%Y%m%d")
         url = f"{self.url_base}/stock/{self.ticker}/chart/{today}/{self.token}"
         self.price_cur = requests.get(url).json()[0]
+        print(self.price_cur[self.price_key])
 
     def set_price_4yr(self):
         today = datetime.date.today()
@@ -86,13 +88,7 @@ class stock_obj():
         today = today.strftime("%Y%m%d")
         url = f"{self.url_base}/stock/{self.ticker}/chart/{today}/{self.token}"
         self.price_4yr = requests.get(url).json()[0]
-
-    def get_time_series(self, data="REPORTED_FINANCIALS"):
-        test_url = f"{self.url_base}/stock/{self.ticker}/quote/latestPrice{self.token}"
-        url = f"{self.url_base}/time-series/{data}/{self.ticker}/{self.token}"
-        response = requests.get(url)
-        for key, val in response.json()[0].items():
-            print(key, val)
+        print(self.price_4yr[self.price_key])
 
     def value_price(self, price_att="price_cur"):
         """
@@ -102,21 +98,50 @@ class stock_obj():
         scores = [(0, 1), (1, 2), (2, 5), (5, 10), (10, 15),
                   (15, 20), (20, 30), (30, 50), (50, 100), (100,999999999)]
         for i, lims in enumerate(scores):
-            price = getattr(self, price_att)["close"]
+            price = getattr(self, price_att)[self.price_key]
             if (price >= lims[0]) and (price < lims[1]):
-                print(price, lims)
                 return i+1
         return 0
+
+    def value_price_trend(self):
+        trend = 100 - ((self.price_cur[self.price_key]*100) / self.price_4yr[self.price_key])
+        print(trend)
+        # scores = dict(percentage_range : att_value)
+        scores = {
+                  (-999999999, -50.5) : 1,
+                  (-50.5, -11.5) : 2,
+                  (-11.5, -0.5) : 5,
+                  (-0.5, 1,5) : 6,
+                  (1.5,10.5) : 7,
+                  (10.5,99.5) : 9,
+                  (99.5,9999999999) : 10
+                } 
+        for lims in scores.keys():
+            if (trend >= lims[0]) and (trend < lims[1]):
+                return scores[lims]
+        return 0
+
+    def get_time_series(self, data="REPORTED_FINANCIALS"):
+        test_url = f"{self.url_base}/stock/{self.ticker}/quote/latestPrice{self.token}"
+        url = f"{self.url_base}/time-series/{data}/{self.ticker}/{self.token}"
+        response = requests.get(url)
+        for key, val in response.json()[0].items():
+            print(key, val)
+
+
 
 def main():
     activate_sandbox_mode()
     stocks = ["AAPL", "TSLA", "MSFT"]
     Tesla = stock_obj(random.choice(stocks), sandbox_mode=True)
+
     print(Tesla.ticker)
     Tesla.set_price_cur()
     Tesla.set_price_4yr()
+
     print(Tesla.value_price("price_cur"))
     print(Tesla.value_price("price_4yr"))
+    print(Tesla.value_price_trend())
 
 if __name__ == "__main__":
     main()
